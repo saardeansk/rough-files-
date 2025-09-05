c2daf6c7bf349851003e2a9910fb3cbaaea6c104
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-05 19:14:09 +0100

    basiccode.cpp

--- basiccode.cpp
+++ basiccode.cpp
@@ -32,3 +32,22 @@ float faren=(celcius*(9/5))+32;
 cout<<"fahrenhit = "<<faren<<endl;
 
 } 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a > b) {
+        cout << "The largest number is: " << a << endl;
+    } else if (b > a) {
+        cout << "The largest number is: " << b << endl;
+    } else {
+        cout << "Both numbers are equal." << endl;
+    }
+
+    return 0;
+}
+
